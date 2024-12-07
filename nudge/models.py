18e20b160e9bd596402e66562791bee9a20c4337
Madlx-devs <mannukumar233@gmail.com>
2024-12-08 00:47:39 +0530

    added few models

--- nudge/models.py
+++ nudge/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class nudges (models.Model):
+    content = models.TextField(max_length=200)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE , related_name='nudges')
+    created_at = models.DateTimeField(auto_now_add=True)
+    images= models.ImageField(upload_to='static/media')
+
+    
+
+    def __str__(self):
+        return self.author
